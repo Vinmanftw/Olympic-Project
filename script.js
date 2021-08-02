ea8e1a1bc2be91e312b87d3e6a095a3bc2d1edfb
Vincent <vao2002@gmail.com>
2021-08-02 16:19:52 -0500

    fetches

--- script.js
+++ script.js
@@ -1,3 +1,86 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
-document.querySelector('body').appendChild(h2); 
+
+
+fetch(
+    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&language=en",
+    {
+        method: "GET",
+        headers: {
+            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
+            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+        },
+    }
+)
+    .then((response) => {
+        return response.json()
+    })
+    .then(console.log)
+
+
+fetch(
+    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=series&genre=18&page=1&language=en",
+    {
+        method: "GET",
+        headers: {
+            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
+            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+        },
+    }
+)
+    .then((response) => {
+        return response.json()
+    }).then(console.log)
+
+
+fetch(
+    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=prime&type=movie&genre=18&page=1&language=en",
+    {
+        method: "GET",
+        headers: {
+            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
+            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+        },
+    }
+)
+    .then((response) => {
+        return response.json()
+    }).then(console.log)
+fetch(
+    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=prime&type=series&genre=18&page=1&language=en",
+    {
+        method: "GET",
+        headers: {
+            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
+            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+        },
+    }
+)
+    .then((response) => {
+        return response.json()
+    }).then(console.log)
+fetch(
+    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=hulu&type=movie&genre=18&page=1&language=en",
+    {
+        method: "GET",
+        headers: {
+            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
+            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+        },
+    }
+)
+    .then((response) => {
+        return response.json()
+    }).then(console.log)
+fetch(
+    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=hulu&type=series&genre=18&page=1&language=en",
+    {
+        method: "GET",
+        headers: {
+            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
+            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+        },
+    }
+)
+    .then((response) => {
+        return response.json()
+    }).then(console.log)
+
