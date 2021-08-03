d938fc6912534e362ea6501b721867b1ba6b6caf
Vincent <vao2002@gmail.com>
2021-08-02 19:17:34 -0500

    Fetches, after remembering DRY

--- script.js
+++ script.js
@@ -1,86 +1,33 @@
+//After Practicing Dry 
 
-
-fetch(
-    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&language=en",
-    {
-        method: "GET",
-        headers: {
-            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
-            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-        },
-    }
-)
-    .then((response) => {
-        return response.json()
-    })
+//sets the http GET object
+const GET = {
+    method: "GET",
+    headers: {
+        "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
+        "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+    },
+}
+//creates a url that thats parameters changes what streaming service and type the data is for
+function url(streamingService,type){
+    return `https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=${streamingService}&type=${type}&genre=18&page=1&language=en`
+}
+//fetches all data from a certain streaming provider (Takes a string ` ` )
+function fetchByProvider(streamingService){
+    fetch(url(streamingService,`movie`),GET)
+    .then((response) => {return response.json()})
     .then(console.log)
-
-
-fetch(
-    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=series&genre=18&page=1&language=en",
-    {
-        method: "GET",
-        headers: {
-            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
-            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-        },
+    fetch(url(streamingService,`series`),GET)
+    .then((response) => {return response.json()})
+    .then(console.log)
+}
+//creates a for loop that runs a parsed function for each element in the Array
+function forLoop(array, fun){ 
+    for(let i = 0;i< array.length;i++){
+        fun(array[i])
     }
-)
-    .then((response) => {
-        return response.json()
-    }).then(console.log)
-
-
-fetch(
-    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=prime&type=movie&genre=18&page=1&language=en",
-    {
-        method: "GET",
-        headers: {
-            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
-            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-        },
-    }
-)
-    .then((response) => {
-        return response.json()
-    }).then(console.log)
-fetch(
-    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=prime&type=series&genre=18&page=1&language=en",
-    {
-        method: "GET",
-        headers: {
-            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
-            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-        },
-    }
-)
-    .then((response) => {
-        return response.json()
-    }).then(console.log)
-fetch(
-    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=hulu&type=movie&genre=18&page=1&language=en",
-    {
-        method: "GET",
-        headers: {
-            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
-            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-        },
-    }
-)
-    .then((response) => {
-        return response.json()
-    }).then(console.log)
-fetch(
-    "https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=hulu&type=series&genre=18&page=1&language=en",
-    {
-        method: "GET",
-        headers: {
-            "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
-            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-        },
-    }
-)
-    .then((response) => {
-        return response.json()
-    }).then(console.log)
-
+}
+//list of the current streaming providers 
+const streamingProviders = [`netflix`, `prime`, `hulu`]
+//returns all off the data from the current streaming providers
+forLoop(streamingProviders,fetchByProvider)
