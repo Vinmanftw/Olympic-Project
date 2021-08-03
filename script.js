9cf6e95b4e6c28501d38adb1f1ffc4ddf06212f1
Vincent <vao2002@gmail.com>
2021-08-03 11:51:19 -0500

    Tab buttons

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const GET = {
     method: "GET",
     headers: {
-        "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
+        "x-rapidapi-key": "571e5b6272mshe8e7cde5770f143p15455cjsne5fac391a610",
         "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
     },
 }
@@ -12,7 +12,7 @@ const GET = {
 function url(streamingService,type){
     return `https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=${streamingService}&type=${type}&genre=18&page=1&language=en`
 }
-//fetches all data from a certain streaming provider (Takes a string ` ` )
+//fetches all data from a certain(SINGLE) streaming provider (Takes a string ` ` ) HTTP tag parameter can be added here in the future
 function fetchByProvider(streamingService){
     fetch(url(streamingService,`movie`),GET)
     .then((response) => {return response.json()})
@@ -27,7 +27,34 @@ function forLoop(array, fun){
         fun(array[i])
     }
 }
+//allow you to run a for loop that parses a function with 2 parameters
+function forLoop2(array, fun, secondPara){ 
+    for(let i = 0;i< array.length;i++){
+        fun(array[i],secondPara)
+    }
+}
+// creates button in requested location gives an id = to name 'div .streamTabs'
+function createButtons(stringName, location){
+    const div = document.createElement(`div`)
+    const bttn = document.createElement('button')
+    bttn.id = `${stringName}`
+    bttn.textContent=`${stringName}`
+    div.appendChild(bttn)
+
+    document.querySelector(`${location}`).appendChild(div)
+}
+// creates an eventlistener at a given location 
+function eventlistener(type, location,fun){
+    document.querySelector(`${location}`).addEventListener(`${type}`,fun())
+}
+
+
 //list of the current streaming providers 
-const streamingProviders = [`netflix`, `prime`, `hulu`]
-//returns all off the data from the current streaming providers
+const streamingProviders = [`Netflix`, `Prime`, `Hulu`]
+//returns ALL off the data from the current streaming providers
 forLoop(streamingProviders,fetchByProvider)
+// create buttons for all streaming providers on homepage 
+forLoop2(streamingProviders,createButtons,`.streamTabs`)
+
+
+
