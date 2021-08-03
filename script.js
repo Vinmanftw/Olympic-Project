faf0bccef578d135543121132123921a39f2a088
Chris <Kirch2@gmail.com>
2021-08-02 21:14:25 -0400

    streamlined JS

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
-document.querySelector('body').appendChild(h2); 
+//After Practicing Dry
+//sets the http GET object
+const GET = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "956f6f9355mshb2c039180194fb0p15f298jsnfaa5a99353d3",
+    "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+  },
+};
+//creates a url that thats parameters changes what streaming service and type the data is for
+function url(streamingService, type) {
+  return `https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=${streamingService}&type=${type}&genre=18&page=1&language=en`;
+}
+//fetches all data from a certain(SINGLE) streaming provider (Takes a string ` ` ) HTTP tag parameter can be added here in the future
+function fetchByProvider(streamingService) {
+  fetch(url(streamingService, `movie`), GET)
+    .then((response) => {
+      return response.json();
+    })
+    .then(console.log);
+  fetch(url(streamingService, `series`), GET)
+    .then((response) => {
+      return response.json();
+    })
+    .then(console.log);
+}
+//creates a for loop that runs a parsed function for each element in the Array
+function forLoop(array, fun) {
+  for (let i = 0; i < array.length; i++) {
+    fun(array[i]);
+  }
+}
+//list of the current streaming providers
+const streamingProviders = [`netflix`, `prime`, `hulu`];
+//returns ALL off the data from the current streaming providers
+forLoop(streamingProviders, fetchByProvider);
